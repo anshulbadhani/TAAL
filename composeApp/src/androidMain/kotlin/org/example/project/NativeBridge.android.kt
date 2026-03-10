0dec44f035baec6c7cf960802e17237508e9e69b
anshulbadhani <anshulbadhani@gmail.com>
2026-03-10 18:24:58 +0530

    Removed the C++ changes

--- composeApp/src/androidMain/kotlin/org/example/project/NativeBridge.android.kt
+++
@@ -1,5 +0,0 @@
-package com.example.myapplication
-
-internal actual object NativeBridge {
-    actual external fun playAsset(assetManager: Any, fileName: String)
-}
--- composeApp/src/commonMain/kotlin/org/example/project/NativeBridge.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication
-
-
-internal expect object NativeBridge {
-    fun playAsset(assetManager: Any, fileName: String)
-}
